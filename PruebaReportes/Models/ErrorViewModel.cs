4e846be7442980980328cb6718da378624fb340e
JJCALEROr <159974772+JJCALEROr@users.noreply.github.com>
2026-05-16 22:07:19 -0600

    Delete PruebaReportes directory

--- PruebaReportes/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PruebaReportes.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
